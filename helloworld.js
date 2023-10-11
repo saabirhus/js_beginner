6865cc62617f43b484d839014f15c84898d8d329
Saabir Hussein <saabirhussein@Saabirs-Air.lan>
2023-10-11 19:24:36 +0100

    this is my first commit ever

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+console.log("Hello world, welcome to JavaScript by Saabir")
+
+//calculate the take salary if the salary is 200 and there is a flat tax of 10
+var salary = 200;
+var tax = 10;
+var takeHome = salary - tax;
+console.log(salary)
+console.log(takeHome)
