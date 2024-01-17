2b64ea23a02882bcd7388fb9b769b65f71b55c35
= <=>
2024-01-17 18:43:45 +0000

    some improvement in my javascript learning

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,126 @@
-console.log("Hello world, welcome to JavaScript by Saabir")
+// console.log("Hello world, welcome to JavaScript by Saabir")
 
-//calculate the take salary if the salary is 200 and there is a flat tax of 10
-var salary = 200;
-var tax = 10;
-var takeHome = salary - tax;
-console.log(salary)
-console.log(takeHome)
-//my name is saabir
+// calculate the take salary if the salary is 200 and there is a flat tax of 10
+// var salary = 200;
+// var tax = 10;
+// var takeHome = salary - tax;
+// console.log(salary)
+// console.log(takeHome)
+// my name is saabir
+
+// var name;
+// name = "Thomas";
+// console.log(name, name, name);
+// var dob = 100;
+// var total = salary * 20;
+// console.log(total);
+
+// var theAddressOfTheLadyNextDoor = "77 Acton Lane";
+
+// var name = "Saabir";
+// console.log(typeof name);
+
+// var numberOfChildren = 6;
+// console.log(typeof numberOfChildren);
+
+// var isMarried = true;
+// console.log(typeof isMarried);
+
+// var price = 25;
+// var cost = 12;
+// var quantitySold = 879;
+// var tax = 6;
+
+// var profit = (price - cost - tax) * quantitySold;
+// console.log(profit)
+
+// var age = 15;
+// if (age <= 4) {
+//   console.log("Go to Nursery");
+// } else if (age > 4 && age <= 11) {
+//   console.log("Go to Primary School");
+// } else if (age > 11 && age <= 16) {
+//   console.log("Go to Secondary School");
+// } else if (age > 16 && age <= 18) {
+//   console.log("Go to Sixth Form");
+// } else {
+//   console.log("Go to University or Work");
+// }
+
+// let num = 5;
+// increment operation
+// num++;
+//decrement operation
+//num--
+//console.log(num);
+
+//is 5 equal to 5 output:true"
+//console.log(5==5)
+//"is 1 equal to 2 output:false"
+// console.log(1 == 2);
+//"is 1 greater than or equal to 1
+
+// console.log(1 === "2");
+
+// 1                    2
+// value 1              value 2
+// data type integer    data type string
+
+//Logical Operators
+
+// == means equal to
+// === means equal value AND equal type
+// != means not equal to
+// !== means not equal value OR not equal type
+// > means greater than
+// < means less than
+// >= means greater than or equal to
+// <= means less than or equal to
+
+// var age = 30; global scope
+
+// {
+//   var age = 15;   local scope
+//   console.log(age);
+// }
+
+// let customerAge = 18;
+
+// if (customerAge >= 18 && customerAge < 65) {
+//   console.log("You're an adult, you can go in and pay");
+// } else if (customerAge < 18) {
+//   console.log("You're a child go home");
+// } else if (customerAge > 65) {
+//   console.log("You're a senior, go in and don't pay");
+// }
+
+// let selectFruit = "Mango";
+
+// switch (selectFruit) {
+//   case "Mango":
+//     console.log("Mango is nice, enjoy it");
+//     break;
+//   case "Banana":
+//     console.log("Somalia has bananas");
+//     break;
+//   case "Orange":
+//     console.log("Oranges are available in Cuba");
+//     break;
+//   default:
+//     console.log("Eat what you want");
+//     break;
+// }
+
+//operation
+
+// let num1 = 20;
+// let num2 = 30;
+// let sum = num1 + num2;
+
+// console.log(sum);
+
+let students = ["Ahmed", "Mohammed", "Ali"];
+// students.push("Farah"); push adds an element to the end of the array
+// students.pop(); pop removes the element at the end of the array
+// let whichGotRemoved = students.shift() shift removes an element from an array and returns it
+console.log(students);
